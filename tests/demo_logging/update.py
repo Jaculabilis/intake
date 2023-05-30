096a594813050d3692b7b264a91b73bec326e2b8
Tim Van Baak <tim.vanbaak@gmail.com>
2023-05-30 14:38:36 -0700

    Make demo_logging pause while writing stdout

--- tests/demo_logging/update.py
+++ tests/demo_logging/update.py
@@ -5,10 +5,17 @@ import os
 import sys
 import time
 
-for i in range(3):
+greeting = os.environ.get("HELLO", "MISSING")
+item = json.dumps({
+    "id": "helloworld",
+    "title": "Hello = " + greeting
+})
+sys.stdout.write(item[:10])
+sys.stdout.flush()
+
+for i in range(5):
     sys.stderr.write(f"{i+1}...\n")
     sys.stderr.flush()
     time.sleep(1)
 
-item = json.dumps({"id": "helloworld", "title": "Hello = " + os.environ.get("HELLO", "MISSING")})
-print(item)
+sys.stdout.write(item[10:])
